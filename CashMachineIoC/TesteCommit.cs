74ef1f3e81bdaa4f194afde790222970a4b364b7
Rusver Alves <rusverav748@gmail.com>
2022-11-30 14:21:07 -0300

    Adicionado atributo

--- CashMachineIoC/TesteCommit.cs
+++ CashMachineIoC/TesteCommit.cs
@@ -9,5 +9,7 @@ namespace CashMachineIoC
         public string Nome { get; set; }
         public string Local { get; set; }
         public string Commit { get; set; }
+        public string Pull { get; set; }
+
     }
 }
